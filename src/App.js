4ac0f9638c02ec719e97915c53166a5e7a4c6346
JulianMorales1 <julian.morales@codeimmersives.com>
2022-09-20 12:11:32 -0400

    upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SignInForm from './components/SignInForm';
+import NewsFeed from './components/NewsFeed';
+import { useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      {user ? <NewsFeed user={user} /> : <SignInForm setUser={setUser} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewsFeed.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const NewsFeed = (props) => {
+
+    const { user } = props;
+
+    const [postBox, setPostBox] = useState('');
+
+    const [listOfPost, setListOfPost] = useState([]);
+
+    const onPost = () => {
+        setListOfPost([...listOfPost, { id: listOfPost.length, content: postBox }]);
+        setPostBox('');
+    };
+
+    const onDeletePost = (postIdToDelete) => () => {
+        setListOfPost(
+            listOfPost.filter(post => post.id !== postIdToDelete)
+        );
+    };
+
+    return (
+        <div>
+            <div>
+                <textarea value={postBox} onChange={(event) => setPostBox(event.target.value)} />
+            </div>
+            <div>
+                <button onClick={onPost}>Post</button>
+            </div>
+            <div>
+                {listOfPost.map((post, index) => (
+                    <div key={index}>
+                        <div>
+                            {user.userName}
+                        </div>
+                        <div>
+                            <img
+                                src={user.profilePhotoUrl}
+                                style={{ width: '50px', height: '50px', objectFit: 'cover' }}
+                            />
+                        </div>
+                        <div>
+                            {post.content}
+                        </div>
+                        <button onClick={onDeletePost(post.id)}>delete</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+};
+
+export default NewsFeed;
